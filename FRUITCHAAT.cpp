b707842b54fe8b53a393c39f8c22ddaf94f6062b
Arunim Malviya <85449742+Arunim313@users.noreply.github.com>
2022-02-11 00:51:53 +0530

    Add files via upload

---
+++ FRUITCHAAT.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(2*y==x) cout<<y<<endl;
+        else{
+            if(2*y>x) cout<<x/2<<endl;
+            else cout<<y<<endl;
+        }
+    }
+	return 0;
+}
