7de2708def9172f1e5834436a9b58b2b07b0141a
Arunim Malviya <85449742+Arunim313@users.noreply.github.com>
2022-03-01 20:33:30 +0530

    Add files via upload

---
+++ NIBBLE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x%4==0) cout<<"Good\n";
+	    else cout<<"Not Good\n";
+	}
+	return 0;
+}
